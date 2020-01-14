90d31f6283d2102be3ef5a0b3c2107485dcd607a
Graham Bewley <grahambewley@gmail.com>
2020-01-14 16:30:38 -0500

    Update live URL

--- utils/baseUrl.js
+++ utils/baseUrl.js
@@ -1,5 +1,5 @@
 const baseUrl = process.env.NODE_ENV === "production" 
-    ? 'https://<my-development-url>.now.sh'
+    ? 'https://centerit.now.sh'
     : 'http://localhost:3000';
 
 export default baseUrl;
